23f1109af8572857045700a50b76ad5c23e9a2ae
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2024-03-21 11:11:58 +0430

    typing effect created

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log("This is application is for learning purposes");
+const div = document.querySelector(".text");
+const text = "Learn Modern JavaScript !";
+function textTypingEffect(element, text, i = 0){
+    if(i == 0){
+        element.textContent = "";
+    }
+    element.textContent += text[i];
+    //if we reached the end of the string 
+    if(i == text.length - 1){
+        return;
+    }
+    setTimeout(() => textTypingEffect(element, text, i + 1),100);
+}
+textTypingEffect(div, text);
+
+
